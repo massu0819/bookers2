2e59331786b931ff0d9a832e38017dab3102507a
massu0819 <massu4869@gmail.com>
2021-10-23 12:15:27 +0000

    [Add]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def index
+    @user = User.all
+  end 
+  
+  def edit
+    @user = User.find(params[:id])
+  end  
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)  
+  end
+  
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index, :edit]
 
 end
