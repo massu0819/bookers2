5490754beece2d3b4f5bb497f7f03d402571ba00
massu0819 <massu4869@gmail.com>
2021-11-03 11:44:43 +0000

    [Add]フォロー・フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
@@ -9,6 +11,9 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
 
-  resources :users, only: [:show, :create, :index, :edit, :update]
-
+  resources :users, only: [:show, :create, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
