eee64b3fca0616fee86592ada2008a51235f94a9
massu0819 <massu4869@gmail.com>
2021-11-04 14:54:40 +0000

    [Add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
+  get '/search', to: 'searchs#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy] do
     resources :book_comments, only: [:create, :destroy]
